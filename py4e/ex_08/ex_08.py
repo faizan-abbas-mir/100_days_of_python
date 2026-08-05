27ac21422c06d278115aff84451906e5aa3eae55
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-08-05 10:41:44 +0000

    list and string operations

--- py4e/ex_08/ex_08.py
+++ py4e/ex_08/ex_08.py
@@ -21,7 +21,7 @@ else:
     print("nope")"""
 
 
-
+"""
 num_list=[]
 new_num=""
 while True:
@@ -35,3 +35,43 @@ total=sum(num_list)
 count=len(num_list)
 average=total/count
 print(average)
+
+asty =[1,2,3,4,5]
+print(asty[1:3])
+"""
+"""x=[]                        #constructor method
+print(type(x))
+#print(dir(x))
+x.append("mango")
+#cannot concatinat a string to list using + 
+print(x)
+if "mango" not in x:
+    print("gugu")
+else:
+    print("gaga")"""
+sentence =" this is a string to be split"
+listed=sentence.split()
+print(listed)
+print(len(listed))
+for w in sentence:
+    print(w)
+for d in listed:
+    print(d)
+
+tricky="split;on;terminator"
+splitedterminator=tricky.split(";")
+print(splitedterminator)
+
+
+fhand=open("test.txt")
+word_list=[]
+for indivisual in fhand:
+    word_list=indivisual.split()
+    if "From" not in word_list:
+        print("nf")
+        continue
+    print(word_list[2])
+    email=word_list[1]
+    splitted_email=email.split("@")
+    print("sendername : "+splitted_email[0])
+    print("domain : "+ splitted_email[1])
