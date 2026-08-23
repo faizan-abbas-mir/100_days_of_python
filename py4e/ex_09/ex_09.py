5d4c6784e96eb4d0542e045e1b20e1cc59eab888
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-08-23 08:58:03 +0000

    dictionary manipulation

--- py4e/ex_09/ex_09.py
+++ py4e/ex_09/ex_09.py
@@ -5,3 +5,14 @@ cabinet['summer']=12
 cabinet['mongo']='db'
 cabinet['summer']+=2
 print(cabinet)
+counts=dict()
+print("enter line of text")
+line=input()
+words=line.split()
+print('words:',words)
+for word in words:
+    counts[word]=counts.get(word,0)+1
+print('count:',counts)
+cunts={'joseph':1,'stephany':33,'mago':67}
+for aaa,bbb in cunts.items():
+    print(aaa,bbb)
