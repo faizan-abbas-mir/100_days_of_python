dec2b81fdbe734939f0de056d9908e58f20c8da0
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-08-23 08:21:51 +0000

    dictionaries start

---
+++ py4e/ex_09/ex_09.py
@@ -0,0 +1,7 @@
+#collections:dictionaries
+#collecion has a key value and an order, however using key is best way to access values
+cabinet= dict()
+cabinet['summer']=12
+cabinet['mongo']='db'
+cabinet['summer']+=2
+print(cabinet)
