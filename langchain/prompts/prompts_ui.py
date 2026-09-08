ee4c0bc70e78c5cb6be568034a32c5e69fed72f7
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-09-08 12:24:22 +0530

    prompts and prompt templates

---
+++ langchain/prompts/prompts_ui.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+import streamlit as st
+
+load_dotenv()
+model = ChatOpenAI(model='gpt-4o-mini')
+
+st.header('research tool')
+
+user_input = st.text_input('Enter your prompt')
+
+if st.button('summarize'):
+    result=model.invoke(user_input)
+    st.write(result.content)
+
+#promptTemplate
+template=PromptTemplate(
+    template="""
+    ejrjjfjbrerbg
+
+
+""",
+input_variables=["input"]
+
+)
+PROMPT=template.invoke({"input": user_input})
+result=model.invoke(PROMPT)
+st.write(result.content)
