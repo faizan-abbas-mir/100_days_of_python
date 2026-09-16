503aede78bf1068409ec247db05ca608027fae9e
faizan-abbas-mir <faixanmir21.fm@gmail.com>
2026-09-16 17:23:03 +0530

    json output parsers

---
+++ langchain/structured_outputs/jsonoutputparser.py
@@ -0,0 +1,29 @@
+#string output parser: gives out string as output always. used over result.content 
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser,JsonOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="conversational",
+    max_new_tokens=512,
+    provider="novita"
+)
+model = ChatHuggingFace(llm=llm)
+parser=JsonOutputParser()
+
+template=PromptTemplate(
+    template="give me the capitals of 5 counties \n {format_instruction}",
+    input_variables=[],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+
+)
+
+prompt=template.format()
+print(prompt)
+result=model.invoke(prompt)
+parsedresult=parser.parse(result.content)
+print(parsedresult)
